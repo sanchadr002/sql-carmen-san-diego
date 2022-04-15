8b6ea275dd8419537c270f100ca4242ef6de0504
sanchadr002 <sanchadr002@gmail.com>
2022-04-15 11:19:29 -0700

    clues 1-3 completed

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -1,19 +1,19 @@
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
-
+SELECT name, population FROM countries WHERE region = 'Southern Europe' AND population = (SELECT MIN(population) FROM countries WHERE region = 'Southern Europe');
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
-
+SELECT language FROM countrylanguages WHERE countrycode = (SELECT code FROM countries WHERE name = 'Holy See (Vatican Cities State)');
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
-
+SELECT name FROM countries WHERE code = (SELECT countrycode from countrylanguages WHERE language = 'Italian' AND percentage = (SELECT MAX(percentage) FROM countrylanguages));
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
